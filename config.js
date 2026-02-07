2a5c783aa38e1869121981b01f045d6d2bfce176
tok163530-eng <tok163530@gmail.com>
2026-02-07 22:04:04 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: ["6030345167"], // ganti jadi id mu
+    botToken: "7217953872:AAEExJ-KvsDr5qI9Xdn9Kp1-vPhW90o9R4U", //isi make token bot mu
+}
+
+module.exports = global;
